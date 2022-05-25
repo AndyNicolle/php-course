7656bff6f20a966582b9b5dfa7b145114300ed75
AndyNicolle <74614479+AndyNicolle@users.noreply.github.com>
2022-05-25 11:50:28 +0100

    Update index.php

--- index.php
+++ index.php
@@ -16,30 +16,28 @@
             $products['Phone']=1000;
             $products['Toaster']=75;
 
-            foreach($products as $key => $value){
-                echo "<p>The ".$key." costs £".$value."</p>";
-            }
-
-            echo "<h2>Items you can afford</h2>";
-
-            foreach($products as $key => $value){
-                if($value <= $credit ){
-                    echo "<p>".$key."</p>";
-                }
-            }
-
-            //$amount=800;
-            //$taxRate=0.0825;
-            //$addedTax= $amount*$taxRate; //amount = 800, tax = 0.0825
-            //echo $addedTax;
+            $taxRate=0.0825;
 
             function tax_calc($amount,$tax){
-                $calculate_tax = $amount*$tax;
-                $amount = round($amount+$calculate_tax,2);
-                return $amount;
+                $addedTax = $amount*$tax;
+                $amountWithTax = round($amount+$addedTax,2);
+                return $amountWithTax;
             }
+            
+                foreach($products as $key => $value){
+                    $costWithTax = tax_calc($value,$taxRate);
+                   echo "<p>The ".$key." costs £".$costWithTax." with tax</p>";
+                }
 
-            echo tax_calc(750,0.223); //Weh you refresh your page you should see the value 917.25 at the bottom of the screen
+                echo "<h2>Items you can afford</h2>";
+
+                foreach($products as $key => $value){
+                    $costWithTax = tax_calc($value,$taxRate);
+                    if($costWithTax <= $credit ){
+                        echo "<p>".$key."</p>";
+                    }
+                }
+
         ?>
     </body>
 </html>
